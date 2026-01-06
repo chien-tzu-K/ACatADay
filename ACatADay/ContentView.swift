77c1bf0ca804952f1d814ec5f33a423f139f2a34
chientzu <j27073021@gmail.com>
2026-01-06 16:46:21 +0800

    ｆｉｒｓｔ

--- ACatADay/ContentView.swift
+++ ACatADay/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let cats = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6"]
+    @State private var selectedCat: String?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.background.ignoresSafeArea()
+                
+
+            if selectedCat != nil {
+                GeometryReader { geo in
+                    Image(selectedCat!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300)
+                        .position(
+                            x: geo.size.width * 0.43,
+                            y: geo.size.height * 0.55
+                        )
+                }
+            }
+
+            GeometryReader { geo in
+                Image(.frontBack)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .position(
+                        x: geo.size.width * 0.5,
+                        y: geo.size.height * 0.8
+                    )
+            }
+
+            GeometryReader { geo in
+                Image(.frontTop)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .position(
+                        x: geo.size.width * 0.5,
+                        y: geo.size.height * 0.13
+                    )
+            }
+
+            GeometryReader { geo in
+                Image(.frontBottom)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 500)
+                    .position(
+                        x: geo.size.width * 0.5,
+                        y: geo.size.height * 0.9
+                    )
+            }
+
+            Button {
+                selectedCat = cats.randomElement()
+            } label: {
+                Image(.catButton)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 350)
+            }
+            .position(x: UIScreen.main.bounds.width / 2, y: 680)
+
         }
-        .padding()
     }
 }
 
