2517dbf0e36c7e45b1eac3fd837edfbb1c1ded47
chientzu <j27073021@gmail.com>
2026-01-06 14:09:20 +0800

    Initial Commit

---
+++ ACatADay/ACatADayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACatADayApp.swift
+//  ACatADay
+//
+//  Created by 孜 on 2026/1/6.
+//
+
+import SwiftUI
+
+@main
+struct ACatADayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACatADay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ACatADay
+//
+//  Created by 孜 on 2026/1/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
